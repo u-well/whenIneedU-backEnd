b2d33df98d1c79a57327ba05284b2521631c1e62
Neota Moe <neota.moe@gmail.com>
2019-05-14 20:51:46 -0500

    updating to make endpoint

--- send-sms.js
+++ send-sms.js
@@ -1,10 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
 const client = require('twilio')(accountSid, authToken);
 
-client.messages.create({
-    to: process.env.MY_PHONE_NUMBER,
-    from: process.env.TWILIO_PHONE_NUMBER,
-    body: 'whenIneedU is here for you!'
-}).then((message) => console.log(message.sid))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, function(){
+    console.log('listening on port: ', port);
+});
+
+app.post('/api/messages', (req, res) => {
+    res.header('Content-Type', 'application/json');
+    client.messages
+      .create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.MY_PHONE_NUMBER,
+        body: "whenIneedU is here for you -- standard message"
+    })
+    .then(() => {
+        res.send(JSON.stringify({ success: true }));
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(JSON.stringify({ success: false }));
+    });
+});
